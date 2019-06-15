8ac19fc0adb105f3b84d78af044e3de08504bf1b
bocheng0802 <51864985+bocheng0802@users.noreply.github.com>
2019-06-16 04:51:34 +0800

    Add files via upload

---
+++ youtube/playList.js
@@ -0,0 +1,14 @@
+console.log(12345678);
+//Youtube 影片 ID
+var playList=[
+    "rBaEMbEziB4",
+    "kkHWFwK84tc",
+    "yoCltsWzUTY"
+];
+
+//播放起迄秒數
+var playTime=[
+    [109,113],
+    [6,10],
+    [0,15]
+];
